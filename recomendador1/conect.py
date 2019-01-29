4ea3cf2a8ec2568586e9d8eb884a1ca291012930
Experimentando y aprendiendo <alfonso_soria_m@hotmail.com>
2019-01-29 18:14:08 +0100

    añadidas cosillas
    añadidos comentarios de intenciones. añadidas ideas a futuros en el readme. Añadido un poco más de código, a falta de depurar.

--- recomendador1/conect.py
+++ recomendador1/conect.py
@@ -21,6 +21,7 @@ class JuezDB:
 
         #Metodo obtener entregas validas de un determinado usuario - Todo (Comprobar correcto funcionamiento)
         def obtenerEntregasValidasDeUser(self, user):
+                        #TODO MEJORAR CONSULTA PARA OBTENER SOLAMENTE LA MEJOR ENTREGA. ADEMAS METERLO EN UNA MATRIZ DE 2 COLUMNAS POR N FILAS. CADA FILA[0] SERA ID DEL PROBLEMA, FILA[1] SERÁ LA POSICIÓN
         		recs = self.cursor.execute('SELECT id FROM submission WHERE user_id = ' + str(user) + ' AND status = AC')
         		listaProblemas = np.empty([recs])
         		i=0
--- recomendador1/recomendador_basico.py
+++ recomendador1/recomendador_basico.py
@@ -1,18 +1,21 @@
-
+import conect
 
 class RecomendadorBasico:
 
-
+	#Crea el recomendador con una conexión a la BBDD y el usuario a recomendar
 	def __init__(self, userIDowner):
 		self.userIDowner = userIDowner
+		self.conexionDB = JuezDB()
 
 	#Devuelve la correlacion entre 2 usuarios
 	def correlacion(user1,user2):
 		#todo
 
-	#Obtencion de los problemas de un usuario X
-	def obtenerProblemas():
+	#Obtencion de los problemas válidos de un usuario X
+	def obtenerProblemas(self, user):
 		#todo
+		#OBTENER LA MATRIZ DE PROBLEMAS/posicion. Obviar la posición de momento a la hora de calcular la correlación.
+		self.conexionDB.obtenerEntregasValidasDeUser(self, user)
 
 	#Obtiene el listado actual de usuarios
 	def obtenerUsuarios():
@@ -20,8 +23,9 @@ class RecomendadorBasico:
 
 
 	#Recomienda a un usuario un lisado de 10 problemas en base al algoritmo de recomendación aplicado.
-	def recomendar(user):
+	def recomendarSimple(user):
 		#todo
+
 	
 	#Devuelve una lista de los usuarios más similares respecto al que se va a recomendar (De cantidad "cantidad")
 	#Esta lista implicará más precisión a la hora de recomendar.

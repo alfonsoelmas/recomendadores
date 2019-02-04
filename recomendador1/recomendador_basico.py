b72d1465b1244047028b949143f949b381385cfe
Experimentando y aprendiendo <alfonso_soria_m@hotmail.com>
2019-02-04 19:41:17 +0100

    Cambios
    Ampliaciones del recomendador y testeos de varios metodos.

--- recomendador1/recomendador_basico.py
+++ recomendador1/recomendador_basico.py
@@ -10,7 +10,6 @@ class RecomendadorBasico:
                 self.listaProblemasOwner = self.obtenerProblemas(self.userIDowner)
 
         #Devuelve la correlacion entre 2 usuarios
-        #Todo: testear
         def correlacion(self,user1):
                 #Anotación: la siguiente busqueda puede darse como una consulta más compleja antes que de forma algoritmica. (comprobar mejora de rendimiento)
                 prob_comunes = self.buscarProblemasComunes(user1) #(pA)intersección(pB)
@@ -23,32 +22,70 @@ class RecomendadorBasico:
                 else:
                         return 0
 
-        #Obtencion de los problemas válidos de un usuario X
+        # Obtencion de los problemas válidos de un usuario X
         def obtenerProblemas(self, user):
                 #Obener Array de problemas.
                 return self.conexionDB.obtenerEntregasValidasDeUser(user)
 
 
-        #Obtiene el listado actual de usuarios
+        # Obtiene el listado actual de usuarios
         def obtenerUsuarios(self):
                 return self.conexionDB.obtenerUsuarios()
 
 
 
-        #Recomienda al usuario problemas en base al algoritmo de recomendación aplicado.
+        # Recomienda al usuario problemas en base al algoritmo de recomendación aplicado.
         def recomendar():
                 return
                 #todo: sin completar
+                #obtener N mas similares.
+                #inicializar array de problemas de tamaño N
+                #para cada problema en concreto (id) su valor "peso" p[i] = p[i] + pesoCorrelUsuariomirando, siendo p[i] un problema concreto del array de problemas y mirando la lista de problemas disjuntos del usuario
+                #obtener problemas de cada usuario
+                #
 
         
-        #Devuelve una lista de los usuarios más similares respecto al que se va a recomendar (De cantidad "cantidad")
-        #Esta lista implicará más precisión a la hora de recomendar.
+        # Devuelve una lista de los usuarios más similares respecto al que se va a recomendar (De cantidad "cantidad")
+        # Esta lista implicará la precisión a la hora de recomendar.
+        # Todo: Testear
         def filtrarNMasSimilares(self,cantidad):
                 listaUsuarios = self.obtenerUsuarios()
+                # Generamos una lista de correlacion asociada a la lista de Usuarios.
+                usuariosCorrel = np.empty([listaUsuarios.size])
+                if cantidad > listaUsuarios.size:
+                        cantidad = listaUsuarios.size
+                i = 0
                 for user in listaUsuarios:
-                        self.correlacion(user)
-                        #todo una vez la correlacion, mirar sus disjuntos y si tiene... añadir a matriz con grado de correlacion y lista de disjuntos
-                #todo: sin completar
+                        correl = self.correlacion(user)
+                        usuariosCorrel[i] = correl
+                        i = i + 1
+                # Ordenamos la lista de correlación y paralelamente el array de IDs de usuario. (Quizas poco óptimo el algoritmo.)
+                i=0
+                while i < usuariosCorrel.size:
+                        j = i
+                        while j < usuariosCorrel.size:
+                                if(usuariosCorrel[j] > usuariosCorrel[i]):
+                                        reserva = usuariosCorrel[i]
+                                        usuariosCorrel[i] = usuariosCorrel[j]
+                                        usuariosCorrel[j] = reserva
+                                        reserva = listaUsuarios[i]
+                                        listaUsuarios[i] = listaUsuarios[j]
+                                        listaUsuarios[j] = reserva
+                                j = j + 1
+                        i = i + 1
+                #Queremos los N usuarios más similares.
+                usuariosCorrelCant = np.empty([cantidad])
+                listaUsuariosCant = np.empty([cantidad],dtype=int)
+                i=0
+                while i<cantidad:
+                        usuariosCorrelCant[i] = usuariosCorrel[i]
+                        listaUsuariosCant[i] = listaUsuarios[i]
+                        i = i + 1
+                #lo transformamos en una matriz de 2 columnas (Fusión de ambos arrays en 1 matriz) (Cada array es una columna)
+                matrizResultado = np.array(usuariosCorrelCant,listaUsuariosCant)
+                matrizResultado.transpose() #Lo trasponemos para hacer que cada columna sea un array y no cada fila sea un array como se genera por defecto.
+                return matrizResultado
+
 
         #Devuelve una lista de problemas comunes entre user2 y user
         def buscarProblemasComunes(self,user2):
@@ -83,7 +120,6 @@ class RecomendadorBasico:
                 return listaFinal
         
         #Devuelve una lista de problemas que tiene user2 y no owner.
-        #todo: Testear
         def buscarProblemasUser2MinusOwner(self, user2):
                 problemasOwner = self.listaProblemasOwner
                 problemasUser2 = self.obtenerProblemas(user2)
@@ -92,23 +128,25 @@ class RecomendadorBasico:
                 #El tamaño máximo de nuestro array comun será como mucho el numero de problemas de user2
                 listaNoComunes = np.empty([problemasUser2.size],dtype=int)
                         
-                #Itero de tal forma que para cada problema del propietario busco en el otro usuario sus problemas. Si está lo añado a la lista y dejo de buscar ese problema.
-                comp = false
+                #Itero de tal forma que para cada problema del otro usuario, busco en el propietario sus problemas. Si está lo añado a la lista y dejo de buscar ese problema.
+                comp = False
                 for problemaUser in problemasUser2:
                         for problemaOwner in problemasOwner:
                                 if problemaOwner == problemaUser:
-                                        comp = true
-                        if comp == false:
+                                        comp = True
+                                if comp == True:
+                                        break
+                        if comp == False:
                                 listaNoComunes[tam] = problemaUser
                                 tam = tam + 1
-                        comp = false
+                        comp = False
                 tamListaFinal = 0
-                listaFinal = np.empty([tam+1]) #Creamos un listado final con el tamaño adecuado
-                while tamListaFinal <= tam:
-                        listaFinal[tamListaFinal] = listaComunes[tamListaFinal]
+                listaFinal = np.empty([tam],dtype=int) #Creamos un listado final con el tamaño adecuado
+                while tamListaFinal < tam:
+                        listaFinal[tamListaFinal] = listaNoComunes[tamListaFinal]
                         tamListaFinal = tamListaFinal + 1
                 return listaFinal
 
 
 recomendador = RecomendadorBasico(847)
-recomendador.buscarProblemasComunes(2742)
+recomendador.correlacion(2742)

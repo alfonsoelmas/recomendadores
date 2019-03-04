8f5cfd21c34dc5bf2cb90362650dcb1be47c45f2
Experimentando y aprendiendo <alfonso_soria_m@hotmail.com>
2019-03-04 17:22:19 +0100

    añadidos
    añadidos mas cosillas. ademas saber que soy el 855 y recomienda bastante coherente.

--- recomendador1v2/analizador.py
+++ recomendador1v2/analizador.py
@@ -14,7 +14,8 @@ leo.readline() #segunda linea se descarta
 
 
 #TODO: ver como guardo estos datos en memoria
-
+fguardo = "resultadosV2_2_reformat.txt"
+guardo = open(fguardo, "w")
 
 #while no lleguemos a fin de documento
 usuarioString   = leo.readline()
@@ -22,16 +23,22 @@ while usuarioString:
     usuarioId       = int(usuarioString[9:len(usuarioString)])
     leo.readline() #"================" Descartamos esta línea
     linea = leo.readline()
+    guardo.write("u: "+ str(usuarioId) +"\n")
     while len(linea)>1 and linea[0]!="[":
         #while no encontremos "[..."
         tamPosProb = linea.find("-->")
         posProb = int(linea[0:tamPosProb])      #pos del problema en matriz
         idProb  = db._problems[posProb]         #id del problema (Traducido)
         pesoProb = float(linea[tamPosProb+3:len(linea)]) #peso del problema
+        guardo.write(str(idProb)+"["+ str(pesoProb) +"]"+"\n")
         linea = leo.readline()
+
     leo.readline() #"====..." Descartamos esta linea
     usuarioString   = leo.readline()
 
 
 
 
+"""
+He aprovechado este programilla para traducir las posiciones a ids de problemas y regenerar un txt con mejor formato
+"""
